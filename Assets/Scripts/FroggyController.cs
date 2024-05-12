35f94e584e1af0837c7738108c1dff8c79b0fb22
ellenlowing <lowing@bu.edu>
2024-05-12 01:17:32 -0700

    both hand control froggy

--- Assets/Scripts/FroggyController.cs
+++ Assets/Scripts/FroggyController.cs
@@ -5,43 +5,62 @@ using UnityEngine.XR;
 
 public class FroggyController : MonoBehaviour
 {
+    public class HandData
+    {
+        public OVRHand Hand;
+        public OVRSkeleton Skeleton;
+        public Transform ThumbTipTransform;
+        public Transform MiddleFingerTipTransform;
+        public bool IsMiddleFingerPinching = false;
+
+        public HandData(OVRHand hand, OVRSkeleton skeleton)
+        {
+            Hand = hand;
+            Skeleton = skeleton;
+            foreach (var b in Skeleton.Bones)
+            {
+                if (b.Id == OVRSkeleton.BoneId.Hand_ThumbTip)
+                {
+                    ThumbTipTransform = b.Transform;
+                }
+                else if (b.Id == OVRSkeleton.BoneId.Hand_MiddleTip)
+                {
+                    MiddleFingerTipTransform = b.Transform;
+                }
+            }
+        }
+    }
+
+    public Transform FroggyParentTransform;
     public Transform FrogTongueTransform;
-    public bool ClawActive = false;
+    public OVRHand FroggyActiveHand = null;
+    public bool FroggyActive = false;
     public float GrabSpeed = 2;
     public float ReturnSpeed = 1;
     public float MaxScaleY = 5f;
-    public XRNode DeviceNode;
+    public OVRHand LeftHand;
+    public OVRHand RightHand;
+    public OVRSkeleton LeftHandSkeleton;
+    public OVRSkeleton RightHandSkeleton;
+    public float MinDistanceToActivateFroggy = 0.08f;
+    public Vector3 FroggyPositionOffset;
+    public Vector3 FroggyRotationOffset;
 
+    private HandData _leftHandData;
+    private HandData _rightHandData;
     private Vector3 _originalFrogTongueScale;
-    private InputDevice _xrController;
-    private bool triggerFired = false;
-    private float triggerValueThreshold = 0.2f;
 
     void Start()
     {
         _originalFrogTongueScale = FrogTongueTransform.localScale;
-        _xrController = InputDevices.GetDeviceAtXRNode(DeviceNode);
+        _leftHandData = new HandData(LeftHand, LeftHandSkeleton);
+        _rightHandData = new HandData(RightHand, RightHandSkeleton);
     }
 
     void Update()
     {
-        // Get controller trigger value
-        if (!_xrController.isValid)
-        {
-            _xrController = InputDevices.GetDeviceAtXRNode(DeviceNode);
-        }
-
-        _xrController.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
-
-        if (!triggerFired && triggerValue > triggerValueThreshold)
-        {
-            triggerFired = true;
-            TriggerPress();
-        }
-        else if (triggerFired && triggerValue <= 0.0f)
-        {
-            triggerFired = false;
-        }
+        UpdateHandData(_leftHandData);
+        UpdateHandData(_rightHandData);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -49,12 +68,64 @@ public class FroggyController : MonoBehaviour
         }
     }
 
+    void UpdateHandData(HandData handData)
+    {
+
+        if (handData.Hand.IsTracked)
+        {
+            handData.IsMiddleFingerPinching = handData.Hand.GetFingerIsPinching(OVRHand.HandFinger.Middle);
+
+            var distanceFromMiddleFingerTipToThumbTip = Vector3.Distance(handData.MiddleFingerTipTransform.position, handData.ThumbTipTransform.position);
+            if (FroggyActiveHand == null && distanceFromMiddleFingerTipToThumbTip < MinDistanceToActivateFroggy)
+            {
+                FroggyActiveHand = handData.Hand;
+                ShowAllRenderers();
+                FroggyParentTransform.parent = FroggyActiveHand.transform;
+                FroggyParentTransform.localPosition = FroggyPositionOffset;
+                FroggyParentTransform.localEulerAngles = FroggyRotationOffset;
+
+                if (FroggyActiveHand == LeftHand)
+                {
+                    FroggyParentTransform.localPosition = -FroggyPositionOffset;
+                    FroggyParentTransform.localEulerAngles = FroggyRotationOffset + new Vector3(0, 0, 180);
+                }
+            }
+            else if (FroggyActiveHand == handData.Hand && distanceFromMiddleFingerTipToThumbTip >= MinDistanceToActivateFroggy)
+            {
+                FroggyActiveHand = null;
+                FroggyParentTransform.parent = null;
+                HideAllRenderers();
+            }
+
+            if (handData.IsMiddleFingerPinching)
+            {
+                TriggerPress();
+            }
+        }
+
+    }
+
+    void ShowAllRenderers()
+    {
+        foreach (var renderer in GetComponentsInChildren<Renderer>())
+        {
+            renderer.enabled = true;
+        }
+    }
+
+    void HideAllRenderers()
+    {
+        foreach (var renderer in GetComponentsInChildren<Renderer>())
+        {
+            renderer.enabled = false;
+        }
+    }
+
     void TriggerPress()
     {
-        if (!ClawActive)
+        if (FroggyActiveHand != null && !FroggyActive)
         {
             StartCoroutine(AnimateFrogTongueScale(_originalFrogTongueScale, new Vector3(1f, MaxScaleY, 1f), GrabSpeed, ReturnSpeed));
-
         }
     }
 
@@ -62,7 +133,7 @@ public class FroggyController : MonoBehaviour
     {
         // PlaySound("Reload");
         float t = 0;
-        ClawActive = true;
+        FroggyActive = true;
         while (t <= 1)
         {
             FrogTongueTransform.localScale = Vector3.Lerp(inScale, outScale, t);
@@ -88,7 +159,7 @@ public class FroggyController : MonoBehaviour
             t += Time.deltaTime * returnSpeed;
             yield return null;
         }
-        ClawActive = false;
+        FroggyActive = false;
         FrogTongueTransform.localScale = inScale;
     }
 

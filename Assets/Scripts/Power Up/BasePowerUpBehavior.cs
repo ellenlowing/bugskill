a0dd6af454c27ff23af6c6c0dba34317eb1cc3b6
ellenlowing <lowing@bu.edu>
2024-05-20 15:53:05 -0700

    define base power up class

---
+++ Assets/Scripts/Power Up/BasePowerUpBehavior.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePowerUpBehavior : MonoBehaviour
+{
+    public enum PowerUpState
+    {
+        IDLE, // not in use + not grabbed
+        INACTIVE, // not in use + grabbed
+        ACTIVE // in use + grabbed
+    }
+
+    public float MaxPowerCapacity = 1;
+    public float PowerCapacity = 1; // [0-1]: indicate battery power of swatter or liquid capacity of spray
+    public float UsePowerRate = 0.001f;
+    public float ChargePowerRate = 0.05f;
+    public PowerUpState CurrentState;
+
+    void Start()
+    {
+        EnterState(PowerUpState.IDLE);
+    }
+
+    void Update()
+    {
+        UpdateState();
+    }
+
+    public void EnterState(PowerUpState state)
+    {
+        switch (state)
+        {
+            case PowerUpState.IDLE:
+                EnterIdleState();
+                break;
+
+            case PowerUpState.INACTIVE:
+                EnterInactiveState();
+                break;
+
+            case PowerUpState.ACTIVE:
+                EnterActiveState();
+                break;
+        }
+
+        CurrentState = state;
+    }
+
+    public void UpdateState()
+    {
+        switch (CurrentState)
+        {
+            case PowerUpState.IDLE:
+                UpdateIdleState();
+                break;
+
+            case PowerUpState.INACTIVE:
+                UpdateInactiveState();
+                break;
+
+            case PowerUpState.ACTIVE:
+                UpdateActiveState();
+                break;
+        }
+    }
+
+    public virtual void EnterIdleState() { }
+    public virtual void UpdateIdleState() { }
+    public virtual void EnterInactiveState() { }
+    public virtual void UpdateInactiveState() { }
+    public virtual void EnterActiveState() { }
+    public virtual void UpdateActiveState()
+    {
+        if (PowerCapacity > 0)
+        {
+            PowerCapacity -= UsePowerRate;
+        }
+    }
+
+    public void Charge()
+    {
+        if (PowerCapacity < MaxPowerCapacity)
+        {
+            PowerCapacity += ChargePowerRate;
+        }
+        else
+        {
+            PowerCapacity = MaxPowerCapacity;
+        }
+    }
+
+}
